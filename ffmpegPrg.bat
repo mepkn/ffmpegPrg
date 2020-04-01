7f51468bf94eb382e74f51c79be7ebe3f5bbb5a1
mepkn <iamthepkn@gmail.com>
2020-04-01 17:18:59 +0530

    Added selection

--- ffmpegPrg.bat
+++ ffmpegPrg.bat
@@ -1,1 +1,16 @@
 @ECHO OFF
+:selection
+ECHO 1 Information
+ECHO 2 Video to Audio
+ECHO 3 Trim
+ECHO 4 Split
+ECHO 5 Join
+ECHO.
+SET /P OPTION="[Enter option ] "
+if %option% == 1 (goto information)
+if %option% == 2 (goto vta)
+if %option% == 3 (goto trim)
+if %option% == 4 (goto split)
+if %option% == 5 (goto join)
+ECHO Unknown value 
+goto selection
